cbd602a59cadc51684c77499f6efe9f065d1c41a
SURYA <suryasaikandikonda@gmail.com>
2019-11-08 16:36:45 +0530

    Updates

---
+++ precache-manifest.36c608383b1eb8de7b8cbcf703ec4c1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8fbb0b1db6f3908a93599cebc884e5",
+    "url": "/pwv4/index.html"
+  },
+  {
+    "revision": "533ef21c478d80d4f9a2",
+    "url": "/pwv4/static/css/main.8c4d396f.chunk.css"
+  },
+  {
+    "revision": "6dc9649cae1a201caba7",
+    "url": "/pwv4/static/js/2.9499ba61.chunk.js"
+  },
+  {
+    "revision": "533ef21c478d80d4f9a2",
+    "url": "/pwv4/static/js/main.875045e8.chunk.js"
+  },
+  {
+    "revision": "ea2382c1d7827a1da0cd",
+    "url": "/pwv4/static/js/runtime-main.8cbd289b.js"
+  },
+  {
+    "revision": "ae475d8e1218f840d2fa24ac99945212",
+    "url": "/pwv4/static/media/Surya Kandikonda Image.ae475d8e.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/pwv4/static/media/logo.25bf045c.svg"
+  }
+]);
